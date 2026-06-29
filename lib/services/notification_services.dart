db4eaefaa9097912bdb657ecc4182ebe65c31964
madhavmuthreja87 <muthrejamadhav@gmail.com>
2026-06-29 17:46:55 +0530

    Scheduling notification almost done

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class NotificationServices {
   final FlutterLocalNotificationsPlugin notifications =
       FlutterLocalNotificationsPlugin();
   Future<void> initialize() async {
+    tz.initializeTimeZones();
     const AndroidInitializationSettings androidSettings =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
@@ -11,6 +16,34 @@ class NotificationServices {
       android: androidSettings,
     );
     await notifications.initialize(settings);
+  }
+
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required DateTime dateTime,
+  }) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+          'medicine_channel',
+          'Medicine Reminder',
+          channelDescription: "Medicine Reminder notification",
+          importance: Importance.max,
+          priority: Priority.high,
+        );
+    const NotificationDetails details = NotificationDetails(
+      android: androidDetails,
+    );
+    final scheduledDate = tz.TZDateTime.from(dateTime, tz.local);
+    await notifications.zonedSchedule(
+      id,
+      title,
+      body,
+      scheduledDate,
+      details,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+    );
   }
 
   Future<void> showNotification() async {
@@ -25,6 +58,7 @@ class NotificationServices {
     const NotificationDetails details = NotificationDetails(
       android: androidDetails,
     );
+
     await notifications.show(
       0,
       '💊 Medicine Reminder',
